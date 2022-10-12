5b57a2f5c9d913cc526f50e50e4332887491dd88
victor aa <victor_993@msn.com>
2022-10-12 12:13:58 +0300

    added additional helper functions that will help generate the cmd

--- helperFunctions.js
+++ helperFunctions.js
@@ -13,9 +13,51 @@ const workspaceGen = (
 const packagesGen = (config) =>
   config.packages.reduce((code, dependency) => {
     return (code += `npm i ${dependency} --force && \\\n`);
-  },"");
+  }, "");
+
+const dependenciesGen = (config) =>
+  config.dependencies.reduce((code, dependecy) => {
+    return (code += `npx nx g ${dependecy}:ng-add --no-interactive && \\\n`);
+  }, "");
+
+const appDirGen = (config, module) =>
+  config.entities.reduce((code, entity) => {
+    return (code += `nx g slice ${entity.modelPlural} \\
+    --project ${module} \\
+    --directory ${entity.modelPlural} \\
+    --no-interactive \\
+    --facade && \\\n`);
+  }, "");
+
+const libGen = (config, suffix) =>
+  config.libs.reduce((code, lib) => {
+    return (code += `nx g lib ${lib} ${suffix} && \\\n`);
+  }, "");
+
+const angularStateGen = (config, module) =>
+  config.entities.reduce((code, entity) => {
+    return (code += `nx g @nrwl/angular:ngrx ${entity.modelPlural} \\
+    --module=libs/${module}/src/lib/${module}.module.ts \\
+    --directory ${entity.modelPlural} \\
+    --no-interactive \\
+    --facade && \\\n`);
+  }, "");
+
+const servicesGen = (config, module) =>
+  config.entities.reduce((code, entity) => {
+    return (code += `nx g s services/${entity.modelPlural}/${entity.modelPlural} --project=${module} && \\\n`);
+  }, "");
+
+const containerComponenetGen = (entity, suffix) =>
+  `nx g c ${entity.modelPlural} ${suffix} && \\\n`
 
 module.exports = {
   workspaceGen,
   packagesGen,
+  dependenciesGen,
+  appDirGen,
+  libGen,
+  angularStateGen,
+  servicesGen,
+  containerComponenetGen,
 };
--- helperFunctions.test.js
+++ helperFunctions.test.js
@@ -34,3 +34,105 @@ test("packages gen works as expected", () => {
 
   expect(res).toBe(expected);
 });
+
+test("dependencies gen works as expected", () => {
+  const config = {
+    dependencies: ["dep1", "dep2"],
+  };
+
+  const res = helper.dependenciesGen(config);
+
+  const expected = `npx nx g dep1:ng-add --no-interactive && \\\nnpx nx g dep2:ng-add --no-interactive && \\\n`;
+
+  expect(res).toBe(expected);
+});
+
+test("app dir gen works as expected", () => {
+  const config = {
+    entities: [
+      {
+        model: "model",
+        modelPlural: "modelPlural",
+      },
+    ],
+  };
+  const module = "module";
+
+  const res = helper.appDirGen(config, module);
+
+  const expected = `nx g slice modelPlural \\
+    --project module \\
+    --directory modelPlural \\
+    --no-interactive \\
+    --facade && \\\n`;
+
+  expect(res).toBe(expected);
+});
+
+test("lib gen works as expected", () => {
+  const config = {
+    libs: ["lib1", "lib2"],
+  };
+
+  const suffix = "suffix";
+
+  const res = helper.libGen(config, suffix);
+
+  const expected = `nx g lib lib1 suffix && \\\nnx g lib lib2 suffix && \\\n`;
+
+  expect(res).toBe(expected);
+});
+
+test("angular state gen works as expected", () => {
+  const config = {
+    entities: [
+      {
+        model: "model",
+        modelPlural: "modelPlural",
+      },
+    ],
+  };
+  const module = "module";
+
+  const res = helper.angularStateGen(config, module);
+
+  const expected = `nx g @nrwl/angular:ngrx modelPlural \\
+    --module=libs/module/src/lib/module.module.ts \\
+    --directory modelPlural \\
+    --no-interactive \\
+    --facade && \\\n`;
+
+  expect(res).toBe(expected);
+});
+
+test("expect services gen to work as expected", () => {
+  const config = {
+    entities: [
+      {
+        model: "model",
+        modelPlural: "modelPlural",
+      },
+    ],
+  };
+  const module = "module";
+
+  const res = helper.servicesGen(config, module);
+
+  const expected = `nx g s services/modelPlural/modelPlural --project=module && \\\n`;
+
+  expect(res).toBe(expected);
+});
+
+test("container gen works as expected", () => {
+  const entity = {
+    model: "model",
+    modelPlural: "modelPlural",
+  };
+  const suffix = "suffix";
+
+  const res = helper.containerComponenetGen(entity, suffix);
+
+  const expected = `nx g c modelPlural suffix && \\\n`;
+
+  expect(res).toBe(expected);
+});
